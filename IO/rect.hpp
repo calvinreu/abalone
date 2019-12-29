e0257838662328ec3712440257e23ffb740f02db
calvinreu <j.calvin.reu@protonmail.com>
2019-12-29 14:20:44 +0100

    changed directory name

--- IO/rect.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <SDL2/SDL.h>
-
-SDL_Rect white_ball = {.x = 100, .y =0, .w = 100, .h = 100};
-SDL_Rect black_ball = {.x = 0, .y =0, .w = 100, .h = 100};
-SDL_Rect selected_ball = {.x = 200, .y =0, .w = 100, .h = 100};
