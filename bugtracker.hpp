f48201b4332740c988149894066a2b4f2946f4cb
calvinreu <j.calvin.reu@protonmail.com>
2019-12-30 00:01:57 +0100

    fixed compiler error

--- bugtracker.hpp
+++ bugtracker.hpp
@@ -2,9 +2,7 @@
 #define movement_call std::cout << "movement called" << std::endl;
 #define mouse_event_getinfo_call std::cout << "mouse event get info called" << std::endl;
 #define render_call std::cout << "renderer called" << std::endl;
-#endif
-
-#ifdef release
+#else
 #define movement_call
 #define mouse_event_getinfo_call
 #define render_call
