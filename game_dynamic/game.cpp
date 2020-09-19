2565b6872edd32655e0077259cb23af7f4ff6f54
calvinreu <calvinreu@localhost.localdomain>
2020-09-19 21:46:50 +0200

    moved board standard to game.cpp

--- game_dynamic/game.cpp
+++ game_dynamic/game.cpp
@@ -1,5 +1,15 @@
 #include "game.hpp"
 
+tile r0[5] = {player0, player0, player0, player0, player0};
+tile r1[6] = {player0, player0, player0, player0, player0, player0};
+tile r2[7] = {empty, empty, player0, player0, player0, empty, empty};
+tile r3[8] = {empty, empty, empty, empty, empty, empty, empty, empty};
+tile r4[9] = {empty, empty, empty, empty, empty, empty, empty, empty, empty};
+tile r5[8] = {empty, empty, empty, empty, empty, empty, empty, empty};
+tile r6[7] = {empty, empty, player1, player1, player1, empty, empty};
+tile r7[6] = {player1, player1, player1, player1, player1, player1};
+tile r8[5] = {player1, player1, player1, player1, player1};
+
 game::game() : board{r0, r1, r2, r3, r4, r5, r6, r7, r8}{}
 
 bool on_board(const position &Tile)
